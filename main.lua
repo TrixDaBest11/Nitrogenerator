051995fa1c5cc8c7c4d6adb25a8677bd859fc418
TrixDaBest11 <142165410+TrixDaBest11@users.noreply.github.com>
2023-08-14 22:21:10 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+local Win = Neverlose_Main:Window({
+    Title = "Neverlose",
+    CFG = "Neverlose",
+    Key = Enum.Keycode.H,
+    External = {
+      Keysystem = true,
+      Key = {
+        "Test",
+        "Beta"
+      }
+    }
+  })
