5e5504340dd28cb078d7070421fd2fd333a381af
TrixDaBest11 <142165410+TrixDaBest11@users.noreply.github.com>
2023-08-14 22:32:34 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+local Neverlose_Main = loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixDaBest11/Nitrogenerator/main/Modules/UISource.lua"))()
 local Win = Neverlose_Main:Window({
     Title = "Neverlose",
     CFG = "Neverlose",
@@ -10,3 +11,45 @@ local Win = Neverlose_Main:Window({
       }
     }
   })
+
+local TabSection1 = win:TSection("Farming")
+
+local tab = TabSection1:Tab("Autofarm")
+
+local sec = tab:Section("Farms")
+local CreditSection = tab:Section("Credits")
+
+CreditSection:Slider("Mana#9724", 0, 1, 0, function() end)
+
+local JoinDiscord = CreditSection:Toggle("Discord", function(t)
+    Neverlose_Main:Notify({
+        Text = "Discord Copied to Clipboard",
+        Time = 2
+    })
+    setclipboard("https://discord.gg/qq6WgyMwkw")
+    hasclicked = t
+    -- local req = (syn and syn.request) or (http and http.request) or http_request or nil
+    -- if req ~= nil then
+    --     for port = 6463, 6472, 1 do
+    --         local inv = 'http://127.0.0.1:' .. tostring(port) .. '/rpc?v=1'
+    --         local http = game:GetService('HttpService')
+    --         local t = {
+    --             cmd = 'INVITE_BROWSER',
+    --             args = {
+    --                 ['code'] = 'PKx8qevgMk'
+    --             },
+    --             nonce = string.lower(http:GenerateGUID(false))
+    --         }
+    --         local post = http:JSONEncode(t)
+    --         req({
+    --             Url = inv,
+    --             Method = 'POST',
+    --             Body = post,
+    --             Headers = {
+    --                 ['Content-Type'] = 'application/json',
+    --                 ['Origin'] = 'https://discord.com'
+    --             }
+    --         })
+    --     end
+    -- end
+end)
